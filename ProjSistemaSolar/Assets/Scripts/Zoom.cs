1a920177fede99c3ec65d102f9f81846f55c6c01
Matheus <theus-7@hotmail.com>
2019-10-31 23:39:09 -0300

    Versão Final

---
+++ ProjSistemaSolar/Assets/Scripts/Zoom.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zoom : MonoBehaviour {
+
+    //This is Main Camera in the Scene
+    Camera m_MainCamera;
+    bool toggle = true;
+
+
+    void Start()
+    {
+        //This gets the Main Camera from the Scene
+        m_MainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+
+            if (toggle == true)
+            {
+                toggle = false;
+               m_MainCamera.fieldOfView = 10;
+               Debug.Log("toggle era p it p 19" + toggle);
+
+            }
+            else
+            {
+                m_MainCamera.fieldOfView = 39;
+                toggle = true;
+                Debug.Log("toggle era p ir p 39" + toggle);
+            }
+    
+    }
+}
